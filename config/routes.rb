d9d7664051ca561ec13a75db9d362c62176c9885
KEVIN-DEVV <kyevenkims@gmail.com>
2023-03-25 16:50:39 +0300

    complete assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+  resources :items, only: [:index, :show]
   resources :users, only: [:show]
+  get '/users', to: 'users#index'
+
 end
